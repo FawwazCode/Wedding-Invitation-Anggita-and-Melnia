0095885a8b5500d08af294e888ea8841325a6905
fawwaz.hirogest <hirogest23@gmail.com>
2026-05-02 11:15:29 +0700

    Tambah Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,6 +53,7 @@ export default function RootLayout({
       <body className={playfair.className + " bg-dark text-white"}>
         {children}
       </body>
+      <Analytics/> 
     </html>
   );
 }
